c2117b8a008f2c2a8b9f8e9557d3fea9040e1930
Branaman <Mbranaman89@gmail.com>
2017-11-21 15:53:07 -0800

    rename

--- MEAN marketplace Project/public/src/app/results-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ResultsFilterPipe } from './results-filter.pipe';
-
-describe('ResultsFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ResultsFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- MEAN marketplace Project/public/src/app/test.ts
+++
@@ -1,8 +0,0 @@
-export class Test {
-  constructor(
-    public test:string='',
-    public score:number=0,
-    public user: string='',
-  ){
-  }
-}
